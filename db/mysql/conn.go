be8828d279efa2b954a63bc71e63d23cdf11a1bb
simonsheng1999 <simonsheng1999@outlook.com>
2021-05-01 11:38:45 +0800

    fix bug

--- db/mysql/conn.go
+++ db/mysql/conn.go
@@ -12,7 +12,7 @@ import (
 var db *sql.DB
 
 func init() {
-	db, _ = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/fileserver?charset=utf8")
+	db, _ = sql.Open("mysql", "root:123456@tcp(127.0.0.1:13306)/fileserver?charset=utf8")
 	db.SetMaxOpenConns(1000)
 	err := db.Ping()
 	if err != nil {
